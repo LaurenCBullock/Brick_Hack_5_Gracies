a81d59756c85121a1700da4d7a0506a13846b489
Skyler <skylerjaneclark@gmail.com>
2019-02-16 20:11:57 -0500

    does some wegmans things?? tries to make a model

--- Assets/Scripts/wegmansJsonGet.cs
+++ Assets/Scripts/wegmansJsonGet.cs
@@ -3,25 +3,19 @@ using LitJson;
 using System;
 using System.Collections;
 
-public class WegmansAPIManager : MonoBehaviour
-{
-    void Start()
-    {
-        Main();
-    }
 
+public class wegmansJsonGet : MonoBehaviour {
 
     public static void Main() {
         var fromJson = "{\"recipes\":[{\"id\":21407,\"name\":\"Asian Cauliflower \\\"Rice\\\" Lettuce Wraps\",\"_links\":[{\"href\":\"/meals/recipes/21407/?api-version=2018-10-18\",\"rel\":\"self\",\"type\":\"GET\"}]},{\"id\":21472,\"name\":\"Beef Fajitas\",\"_links\":[{\"href\":\"/meals/recipes/21472/?api-version=2018-10-18\",\"rel\":\"self\",\"type\":\"GET\"}]}]}";
-        RecipeList fromObject = RecipeList.CreateFromJSON(fromJson);
-        Debug.Log(fromObject);
+        var fromObject = LitJson.JsonMapper.ToObject<RecipeList>(fromJson);
+        Debug.Log(fromJson);
     }
 
     [System.Serializable]
     public class RecipeList
     {
-        public Recipe recipes;
-
+        public Recipe recipe;
         public static RecipeList CreateFromJSON(string json)
         {
             return JsonUtility.FromJson<RecipeList>(json);
